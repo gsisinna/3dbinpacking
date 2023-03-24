4dcef7c6fe8927dc0529a25d80bcfd6ac3afacb2
Gabriele Sisinna <gabriele.sisinna@gmail.com>
2023-03-24 11:18:07 +0100

    Update setup.py to fix deprecated warnings

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
+"""Setuptools-based setup module for the py3dbp package."""
+
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='py3dbp',
     version='1.1.2',
-    author="Enzo Ruiz Pelaez",
-    author_email="enzo.rp.90@gmail.com",
-    description="3D Bin Packing",
+    author='Enzo Ruiz Pelaez',
+    author_email='enzo.rp.90@gmail.com',
+    description='3D Bin Packing',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/enzoruiz/3dbinpacking",
+    long_description_content_type='text/markdown',
+    url='https://github.com/enzoruiz/3dbinpacking',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
